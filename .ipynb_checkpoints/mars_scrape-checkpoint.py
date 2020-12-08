6b25dc342ed3f789ec5a9318d8b4e85f5c3cc259
Pmhu4242 <46797343+pmhu4242@users.noreply.github.com>
2020-12-07 18:10:49 -0700

    Intial Work on Assignment Draft of code for app.py

---
+++ .ipynb_checkpoints/mars_scrape-checkpoint.py
@@ -0,0 +1,25 @@
+from splinter import Browser
+from bs4 import BeautifulSoup
+from webdriver_manager.chrome import ChromeDriverManager
+
+def init_browser():
+    # @NOTE: Replace the path with your actual path to the chromedriver
+    executable_path = {"executable_path": ChromeDriverManager().install()}
+    return Browser("chrome", **executable_path, headless=True)
+
+
+def scrape():
+    browser = init_browser()
+    
+
+    url = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
+    browser.visit(url)
+
+    html = browser.html
+    soup = BeautifulSoup(html, "html.parser")
+
+    # listings["headline"] = soup.find("a", class_="result-title").get_text()
+    # listings["price"] = soup.find("span", class_="result-price").get_text()
+    # listings["hood"] = soup.find("span", class_="result-hood").get_text()
+
+    # return listings
